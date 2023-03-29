ef88734166411e7a66b1ee5e5ce12b85b6a3d804
Gabriel Dickmann Schneider <TheCueioNerd@gmail.com>
2023-03-28 22:05:55 -0300

    Site

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onscroll = function() {
+  var nav = document.getElementsByTagName('nav')[0];
+  if (window.pageYOffset > 0) {
+      nav.classList.add('fixed');
+  } else {
+      nav.classList.remove('fixed');
+  }
+}
+
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function() {
+  navigation.classList.toggle('active');
+  main.classList.toggle('active');
+}
+
+let list = document.querySelectorAll('.navigation li');
+function activeLink(){
+  list.forEach((item) => item.classList.remove('hovered'));
+  this.classList.add('hovered');
+}
+list.forEach((item) => item.addEventListener('mouseover', activeLink));
